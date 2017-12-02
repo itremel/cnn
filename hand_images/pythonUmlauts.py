ce686c7bbaa279804c03fcbfad2deb8fc9ace8fe
Ivo <ivo.tremel@web.de>
2017-12-02 12:25:02 +0100

    fixed indentation

--- hand_images/pythonUmlauts.py
+++ hand_images/pythonUmlauts.py
@@ -1,12 +1,11 @@
 import os
 import re
-#das hier kann bei Python3 eigentlich raus weil das eh Standardmäßig drin ist 
 
 #xccx88 is the dots over A,O,U --> ÄÖÜ
 p = re.compile(b'\xcc\x88')
 
 for filename in os.listdir(b'.'):
- 	   m = p.search(filename)
+    m = p.search(filename)
     if(m!=None):
         print(repr(m.group()))
         print(filename)
