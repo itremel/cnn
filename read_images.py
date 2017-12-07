89e8ac403447fb33974941a300021de9670ea7fe
LNoehren <noehren@sra.uni-hannover.de>
2017-12-07 14:16:17 +0100

    added crop stuff

--- read_images.py
+++ read_images.py
@@ -10,7 +10,9 @@ def _parse_function(filename, label):
   image_string = tf.read_file(filename)
   image_decoded = tf.image.decode_image(image_string, channels=1)
   image_decoded.set_shape([384, 288, 1])
-  image_resized = tf.image.resize_images(image_decoded, [28, 28])
+  #image_cropped = tf.image.central_crop(image_decoded, 0.5)
+  image_cropped = tf.image.crop_to_bounding_box(image_decoded, 70, 125, 150, 150)
+  image_resized = tf.image.resize_images(image_cropped, [28, 28])
   
   #image = sess.run(image_resized)
   #plt.imshow(image_decoded)
@@ -39,24 +41,36 @@ for filename in os.listdir('hand_images/'):
     for regex, label in regexLabelList:
         if re.search(regex, filename) != None:
             imageFilenames.append('hand_images/' + filename)
-            print('hand_images/' + filename)
+            #print('hand_images/' + filename)
             imageLabels.append(label)
-            print(label)
+            #print(label)
+
+for i in range(len(imageFilenames)):
+    print(imageFilenames[i], imageLabels[i])
 
 # A vector of filenames.
 filenames = tf.constant(imageFilenames)
 
-# `labels[i]` is the label for the image in `filenames[i].
+# labels[i] is the label for the image in filenames[i].
 labels = tf.constant(imageLabels)
 
 dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
 dataset = dataset.map(_parse_function)
 
-iterator = dataset.make_one_shot_iterator()
-for _ in range(80):
+max_value = tf.placeholder(tf.int64, shape=[])
+iterator = dataset.make_initializable_iterator()
+sess.run(iterator.initializer, feed_dict={max_value: 1000})
+
+pos = np.random.randint(0, high=1000)
+
+for _ in range(pos):
     sess.run(iterator.get_next())
 tensor = iterator.get_next()
-print(sess.run(tensor)[1])
-plt.imshow(np.reshape(sess.run(tensor)[0], [28,28]))
+
+bla = sess.run(tensor)
+
+print(imageFilenames[pos])
+print(bla[1])
+plt.imshow(np.reshape(bla[0], [28,28]))
 plt.show()
 
