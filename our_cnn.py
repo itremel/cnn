e5096a043fd5936272f30967a2b8c190da689816
LNoehren <noehren@sra.uni-hannover.de>
2017-12-22 16:14:53 +0100

    aufgeräumt, in jedem step wird 1 batch gemacht statt ganzes dataset

--- our_cnn.py
+++ our_cnn.py
@@ -11,7 +11,19 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+#set to True to use inception Module, Flase to use convolution and max pooling
 use_inception = True
+
+#set True to restore a previous Model
+use_previous = False
+#file path to previous Model
+file_path = os.getcwd()+'/model.ckpt'
+
+#batchsize for the test and training Data
+batchsize = 50
+
+#number of training steps for the Model
+num_steps = 100
 
 #creates a cnn with one convolution layer one max pooling layer and one fully connected layer
 def cnn(x):
@@ -96,19 +108,6 @@ def cnn_inception(x):
         
         #concatenate the feature maps
         h_inception = tf.nn.relu(tf.concat([h_conv1_1x1_1, h_conv1_3x3, h_conv1_5x5, h_conv1_1x1_4], 3))
-    
-    #convolution and max pooling is replaced by inception layer
-    ##################
-    # Convolutional layer - maps 1 input image to 32 feature maps.
-    with tf.name_scope('conv'):
-        W_conv = weight_variable([5, 5, 1, 32])
-        b_conv = bias_variable([32])
-        h_conv = tf.nn.relu(conv2d(x_image, W_conv) + b_conv)
-      
-    # Pooling layer - downsamples by 2X.
-    with tf.name_scope('pool'):
-        h_pool = max_pool_2x2(h_conv)
-    ##################
         
     # Fully connected layer 1 - after inception layer
     with tf.name_scope('fc1'):
@@ -188,7 +187,7 @@ def create_dataset():
     for filename in os.listdir('hand_images/'):
         for regex, label in regexLabelList:
             if re.search(regex, filename) != None:
-                if counter < 500:
+                if counter%2 == 0:
                     imageFilenames.append('hand_images/' + filename)
                     imageLabels.append(label)
                 else:
@@ -205,42 +204,25 @@ def create_dataset():
     dataset = dataset.map(_parse_function)
     testdataset = tf.data.Dataset.from_tensor_slices((testfilenames, testlabels))
     testdataset = testdataset.map(_parse_function)
-    #iterator = dataset.make_one_shot_iterator()
-    #features, labelsS = iterator.get_next()
-    #features = tf.reshape(features, [-1,784])
-    #labelsS = tf.reshape(labelsS, [-1,784])
 
     return dataset, testdataset
 
 def main():
+    #create datasets and iterators
     dataset, testdataset = create_dataset()
     print("created dataset")
-    batchsize=100
+    
     batched_dataset = dataset.batch(batchsize)
     it = batched_dataset.make_initializable_iterator()
-    next_element = it.get_next()
+    next_batch = it.get_next()
     
     batched_test_data = testdataset.batch(batchsize)
-    #iterator = batched_dataset.make_one_shot_iterator()
-    #next_element = iterator.get_next()
-    #with tf.Session() as sess:
-        #print(sess.run(next_element)[1])
-        #print(sess.run(next_element)[1])
-        #print(sess.run(next_element)[1])
-    #with tf.Session() as sess:
-        #for _ in range(100):
-            #sess.run(it.initializer)
-        #while True:
-            #try:
-                #print(sess.run(next_element)[1])
-                #print("\n")
-            #except tf.errors.OutOfRangeError:
-                #break
-    #return;
+    test_it = batched_test_data.make_initializable_iterator()
+    next_test_batch = test_it.get_next()
+    
     # Create the model
+    
     x = tf.placeholder(tf.float32, [None, 784])
-
-    # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
 
     # Build the graph for the deep net
@@ -251,16 +233,20 @@ def main():
         print('not using inception layer')
         y_conv = cnn(x)
     
+    #define loss and optimizer
     with tf.name_scope('loss'):
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_,
                                                             logits=y_conv)
     cross_entropy = tf.reduce_mean(cross_entropy)
     tf.summary.scalar('cross_entropy', cross_entropy)
 
-
     with tf.name_scope('adam_optimizer'):
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
+        
+    with tf.name_scope('test_prediction'):
+        test_prediction = tf.nn.softmax(y_conv)
+        
+    #define accuracy for training and test data
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
         correct_prediction = tf.cast(correct_prediction, tf.float32)
@@ -272,97 +258,63 @@ def main():
     print('Saving graph to: %s' % graph_location)
     train_writer = tf.summary.FileWriter(graph_location)
     train_writer.add_graph(tf.get_default_graph())
+    
+    saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        #print(sess.run(features))
-        #image=np.array(sess.run(features))
-        #label=np.array(sess.run(labels))
-        #print(image)
-        for i in range(5):
-            sess.run(it.initializer)
-            
+        
+        #use previos model
+        if use_previous:
+            saver.restore(sess, file_path)
+            print('previous Model restored')
+        
+        #training
+        print('starting training')
+        sess.run(it.initializer)
+        for i in range(num_steps):       
             train_accuracy = []
             
-            #batch = mnist.train.next_batch(50)
-            #batch = data.batch(50)
-            #iterator = dataset.make_one_shot_iterator()
-            #next_element = iterator.get_next()
-            print('starting training step', i)
-            while True:
-                try:
-                    #print(sess.run(next_element)[1])
-                    element=sess.run(next_element)
-                    image = np.reshape(element[0], [batchsize,784])
-                    #print(image)
-                    label = np.reshape(element[1],[batchsize,10])
-                    #   print(label)
-                    train_step.run(feed_dict={x: image, y_: label})
-                    train_accuracy.append(accuracy.eval(feed_dict={x: image, y_: label}))
+            try:
+                batch = sess.run(next_batch)
+                image = np.reshape(batch[0], [batchsize,784])
+                label = np.reshape(batch[1],[batchsize,10])
+                   
+                train_step.run(feed_dict={x: image, y_: label})
+                summary, acc = sess.run([merged, accuracy], feed_dict={x:image, y_:label})
+                train_accuracy.append(acc)
+                
+                train_writer.add_summary(summary, i)
 
-                except tf.errors.OutOfRangeError:
-                    break
-            #make summary calculation over all 500 elements of the training set
-            whole_batched_dataset = dataset.batch(500)
-            summary_iterator = whole_batched_dataset.make_one_shot_iterator()
-            summary_next_example, summary_next_label = sess.run(summary_iterator.get_next())
-            #print(summary_iterator.output_shapes)
-            summary, acc = sess.run([merged, accuracy], feed_dict={x: np.reshape(summary_next_example,[500,784]), y_: np.reshape(summary_next_label,[500,10])})
-            train_writer.add_summary(summary, i)
-
-            avg_accuracy = np.sum(train_accuracy)/len(train_accuracy)
-            print('step %d, average training accuracy %g %g' % (i, avg_accuracy, acc))
-        #train_writer.close()
-    #iterator = dataset.make_one_shot_iterator()
-    #next_element = iterator.get_next()
-    #while True:
-        #try:
-            #print(sess.run(next_element[0]))
-            #element=sess.run(next_element)
-            #image = np.reshape(element[0], [1000,784])
-            #label = np.reshape(element[1],[1000,10])
-            #print('test accuracy %g' % accuracy.eval(feed_dict={x: image, y_: label}))
-        #except tf.errors.OutOfRangeError:
-            #break
-        max_value = tf.placeholder(tf.int64, shape=[])
-        iterator = batched_test_data.make_initializable_iterator()
-        sess.run(iterator.initializer, feed_dict={max_value: 500})
-        next_test_element = iterator.get_next()
-
-        #pos = np.random.randint(0, high=500)
-        #print("find random postion....")
+                avg_accuracy = np.sum(train_accuracy)/len(train_accuracy)
+                print('step %d, average training accuracy %g' % (i, avg_accuracy))
+            
+            except tf.errors.OutOfRangeError:
+                sess.run(it.initializer)
+                i -= 1
+                    
+            
+        #tests
+        sess.run(test_it.initializer)      
         test_accuracy = []
         
         print('starting testruns')
         while True:
-            try:
-                #print(sess.run(next_element)[1])
-                test_element=sess.run(next_test_element)
-                image = np.reshape(test_element[0], [batchsize,784])
-                #print(image)
-                label = np.reshape(test_element[1], [batchsize,10])
-                #print(y_conv, label)
+            try:                
+                test_batch=sess.run(next_test_batch)
+                image = np.reshape(test_batch[0], [batchsize,784])
+                label = np.reshape(test_batch[1], [batchsize,10])
+                
                 test_accuracy.append(accuracy.eval(feed_dict={x: image, y_: label}))
+                
             except tf.errors.OutOfRangeError:
                 break
+                
         avg_accuracy = np.sum(test_accuracy)/len(test_accuracy)
         print('average test accuracy %g' % avg_accuracy)
         
-        
-        #for _ in range(pos):
-         #   sess.run(iterator.get_next())
-        #tensor = iterator.get_next()
-
-        #bla = sess.run(tensor)
-        #image = np.reshape(bla[0], [1,784])
-        ##print(image.shape)
-        #label = np.reshape(bla[1],[1,10])
-        #print('test accuracy %g' % accuracy.eval(feed_dict={x: image, y_: label}))
-        #print(bla[0].shape)
-        #print(bla[1])
-        #plt.imshow(np.reshape(bla[0], [28,28]))
-        #plt.show()
-
+        saver.save(sess, file_path)
+        print('Model saved')
             
 if __name__ == '__main__':
     main()
